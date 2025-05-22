f64a1f4891aa8bb5887d5c3b874d55b74dab6a04
Richard <richardchoi54@gmail.com>
2025-05-22 18:25:02 -0400

    initial clean start of richard-v yes-chef application

--- server/api/distributor/distributorRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getDistributorIngredients } from './distributorController.ts';
-
-const router = express.Router();
-
-// router.get('/api/distributor', getDistributorIngredients);
-router.get('/api/distributor/search/:query', getDistributorIngredients);
-
-export default router;
