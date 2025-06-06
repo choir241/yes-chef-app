fd067f68feb9e4150d3d5d65143308b5cf7a39ce
Richard <richardchoi54@gmail.com>
2025-06-06 09:46:42 -0400

    init: initialize storybook and remove boilerplate files. kept one for both components and docs to refer for example

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+
+
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-design-assets",
+    '@storybook/addon-links'
+  ],
+  "framework": {
+    "name": "@storybook/react-vite",
+    "options": {}
+  }
+};
+export default config;
